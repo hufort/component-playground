d04d950c599ea77718c390d6d49b444785ed8a7d
hufort <hugh.forte@gmail.com>
2023-12-12 16:47:46 -0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <button>Button</button>
+    </div>
+  )
+}
+
+export default App
